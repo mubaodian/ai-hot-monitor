6674fcbd46742fc11c04c708acc109a24bffc14f
1350147451@qq.com <1350147451@qq.com>
2026-09-24 16:25:02 +0800

    修改代理配置

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   publicDir: false,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true
+      }
+    }
+  },
   build: {
     outDir: 'public',
     emptyOutDir: true
